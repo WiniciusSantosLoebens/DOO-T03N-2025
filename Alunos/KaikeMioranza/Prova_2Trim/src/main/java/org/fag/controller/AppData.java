199db04329ad1d4b8b7b8bca5012a40c1e2a16ef
kaikecod <kaikemioranza@outlook.com>
2025-06-22 21:55:00 -0300

    Prova 2Tri

---
+++ Alunos/KaikeMioranza/Prova_2Trim/src/main/java/org/fag/controller/AppData.java
@@ -0,0 +1,55 @@
+package org.fag.controller;
+
+import org.fag.model.Serie;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppData {
+    private String userName;
+    private List<Serie> favorites;
+    private List<Serie> watched;
+    private List<Serie> watchlist;
+
+    public AppData() {
+        this.favorites = new ArrayList<>();
+        this.watched = new ArrayList<>();
+        this.watchlist = new ArrayList<>();
+    }
+    public AppData(String userName, List<Serie> favorites, List<Serie> watched, List<Serie> watchlist) {
+        this.userName = userName;
+        this.favorites = favorites != null ? new ArrayList<>(favorites) : new ArrayList<>();
+        this.watched = watched != null ? new ArrayList<>(watched) : new ArrayList<>();
+        this.watchlist = watchlist != null ? new ArrayList<>(watchlist) : new ArrayList<>();
+    }
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<Serie> getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(List<Serie> favorites) {
+        this.favorites = favorites;
+    }
+
+    public List<Serie> getWatched() {
+        return watched;
+    }
+
+    public void setWatched(List<Serie> watched) {
+        this.watched = watched;
+    }
+
+    public List<Serie> getWatchlist() {
+        return watchlist;
+    }
+
+    public void setWatchlist(List<Serie> watchlist) {
+        this.watchlist = watchlist;
+    }
+}
---
+++ Alunos/KaikeMioranza/Prova_2Trim/src/main/java/org/fag/controller/comparators/SerieRatingComparator.java
@@ -0,0 +1,15 @@
+package org.fag.controller.comparators;
+
+import org.fag.model.Serie;
+
+import java.util.Comparator;
+
+public class SerieRatingComparator implements Comparator<Serie> {
+    @Override
+    public int compare(Serie s1, Serie s2) {
+        Double rating1 = (s1 != null && s1.getNotaGeral() != null) ? s1.getNotaGeral() : 0;
+        Double rating2 = (s2 != null && s2.getNotaGeral() != null) ? s2.getNotaGeral() : 0;
+
+        return rating1.compareTo(rating2);
+    }
+}
